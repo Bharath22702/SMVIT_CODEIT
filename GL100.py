9e6b8c886cdb9d613c99a81e838de7a69d0bb827
Bharath22702 <78546785+Bharath22702@users.noreply.github.com>
2021-02-06 15:21:02 +0530

    Create GL100.py

---
+++ GL100.py
@@ -0,0 +1,10 @@
+a = float(input('Enter first number :'))
+b = float(input('Enter second number:'))
+c = float(input('Enter third number :'))
+d = a + b
+e = d * c
+print(e) 
+if e > 100;
+    print('Greater than 100')
+else:
+    print('Lesser than 100')
