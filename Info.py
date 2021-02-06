22727608032e444f526f7e7ec6c16d02c23741f8
Bharath22702 <78546785+Bharath22702@users.noreply.github.com>
2021-02-06 14:56:09 +0530

    Create Info.py

---
+++ Info.py
@@ -0,0 +1,6 @@
+NAME = input('Enter your name : ')
+AGE = int(input('Enter your age : '))
+SEX = input('Enter your sex : ' )
+print('Name : ' + NAME)
+print('Age  : ' + str(AGE))
+print('Sex  : ' + SEX)
